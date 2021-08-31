5f487028487524826ab76990ef89dc2d14bb548d
Nathaniel Riley <38742706+actuallyitsnathaniel@users.noreply.github.com>
2021-08-31 12:24:34 -0700

    Add files via upload

---
+++ App/components/Button.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "rgba(255, 255, 255, 0.3)",
+    borderRadius: 10,
+    paddingVertical: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "46%",
+    marginTop: 20
+  },
+  text: {
+    color: "#fff",
+    fontSize: 20,
+    textAlign: "center"
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginTop: 20,
+    justifyContent: "space-between"
+  }
+});
+
+export const Button = ({ text, onPress = () => {} }) => (
+  <TouchableOpacity onPress={onPress} style={styles.button}>
+    <Text style={styles.text}>{text}</Text>
+  </TouchableOpacity>
+);
+
+// holds a collection of buttons in a specific format
+export const ButtonContainer = ({ children }) => (
+  <View style={styles.buttonContainer}>{children}</View>
+);
